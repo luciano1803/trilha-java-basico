beb7a0342609c4a45fe5161960f31394560759fd
lorenzo ridan <joseramosbtc@gmail.com>
2024-05-18 08:50:22 -0300

    commit do projeto conta bancaria

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Conhecer e importar a classe Scanner
+       
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.println("Por favor, digite o número da Agência:");
+        
+        String agencia = scanner.nextLine();
+          //Exibir a mensagem conta criada
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); 
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+        
+        //Exibir as mensagem para o usuario
+       
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+       
+       
+    
     }
 }
